2151c75b69371c8b304de53d900b9cfef6ec2094
Theodore Oliver <144561651+Turtlewords@users.noreply.github.com>
2025-10-18 21:23:38 +0000

    begin writing app

--- app.rb
+++ app.rb
@@ -14,13 +14,34 @@ get("/search_by_title") do
 end
 
 get("/search_by_lyrics") do
+ 
+
   erb(:search_by_lyrics)
 end
 
 get("/found_lyrics") do
+
+   @artist = params.fetch("artist")
+   @title = params.fetch("title")
+
+   response = HTTP.get("https://api.lyrics.ovh/v1/#{@artist}/#{@title}")
+
+   @data = JSON.parse(response)
+
   erb(:found_lyrics)
 end
 
 get("/found_title") do
+
+   c = AI::Chat.new
+  
+  c.system("You are a helpful assistant who identifies musical artists and song titles from lyrics")
+
+  @lyrics = params.fetch("lyrics")
+
+  c.user(@lyrics)
+
+  @ai_reply = c.generate!
+
   erb(:found_title)
 end
